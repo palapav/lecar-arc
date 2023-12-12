4bace034971ba3cc9817282bc7e235e28e06e05f
Matt Drapkin <matt@drapkin.org>
2023-12-12 16:16:30 -0500

    Updated run.py to handle CL args, must integrate LeCaR once built

--- run.py
+++ run.py
@@ -1,21 +1,26 @@
 
-import sys
+import argparse
 from arccache import ARCCache
 
-if __name__ == '__main__':
-    c = int(sys.argv[1])
-    requests_file = sys.argv[2]
+if __name__ == '__main__':# Create an ArgumentParser object
+    parser = argparse.ArgumentParser(description='ARC vs LeCaR Cache Evaluation')
+    parser.add_argument('algorithm', choices=['lecar', 'arc'], help='Caching algorithm (lecar or arc)')
+    parser.add_argument('capacity', type=int, help='Cache capacity')
+    parser.add_argument('trace_file', type=str, help='Trace file name')
+
+    args = parser.parse_args()
     requests, hits = 0, 0
 
-    arc_cache = ARCCache(c)
-    with open(requests_file, 'r') as f:
+    arc_cache = ARCCache(args.capacity)
+    with open(args.trace_file, 'r') as f:
         for line in f:
             requests += 1
 
+            # determine cache to use here once LeCaR built
             hit = arc_cache.request(int(line))
 
             if hit:
                 hits += 1
 
-        print("ARC Stats: %d hits out of %d requests. Hit-rate = %.2f%".format() %
-                (hits, requests, round(hits / requests * 100, 2)))
+        print(f'ARC Stats: {hits} hits out of {requests} requests. ' +
+              f'Hit-rate = {round(hits / requests * 100, 2)}%')
